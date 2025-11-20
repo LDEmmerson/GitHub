eaffe39dab9931a68359855916ad3d2ea40d8fa7
Liam Emmerson <Liam.Emmerson1@hotmail.com>
2025-11-20 20:24:05 +0000

    Removed the unity project tile due to issue with the files and structure uploading to github

--- Projects/RPG_Project/obj/Debug/net9.0/RPG_Project.AssemblyInfo.cs
+++ Projects/RPG_Project/obj/Debug/net9.0/RPG_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b5dfa8509088b9850faf6ceb88961d2220def81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c03e2ab214775bd83114d464295688e14b8f8eb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
